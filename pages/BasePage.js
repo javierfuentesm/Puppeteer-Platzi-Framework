f486487559aa94f4d589e5d61a2930b54e7d465d
Javier <javier.fuentesm@hotmail.com>
2021-11-11 00:20:49 -0500

    Base Page

---
+++ pages/BasePage.js
@@ -0,0 +1,64 @@
+export default class BasePage {
+	async getTitle() {
+		return await page.title()
+	}
+
+	async getUrl() {
+		return await page.url()
+	}
+
+	async getText(selector) {
+		try {
+			await page.waitForSelector(selector)
+			return await page.$eval(selector, (el) => el.textContent)
+		} catch (e) {
+			throw new Error(`Error al obtener el texto del selector ${selector}`)
+		}
+	}
+
+	async getAttribute(selector, attribute) {
+		return await page.$eval(selector, (el) => el.getAttribute(attribute))
+	}
+
+	async getValue(selector) {
+		return await page.$eval(selector, (el) => el.value)
+	}
+
+	async getCount(selector) {
+		try {
+			await page.waitForSelector(selector)
+			return await page.$$eval(selector, (el) => el.length)
+		} catch (e) {
+			throw new Error(`Error al obtener el numero de elementos del selector ${selector}`)
+		}
+	}
+
+	async click(selector) {
+		try {
+			await page.waitForSelector(selector)
+			await page.click(selector, opts)
+		} catch (e) {
+			throw new Error(`Error al dar click en el selector ${selector}`)
+		}
+	}
+
+	async type(selector, text, opts = {}) {
+		try {
+			await page.waitForSelector(selector)
+			await page.type(selector, text, opts)
+		} catch (e) {
+			throw new Error(`Error al escribir en el selector ${selector}`)
+		}
+	}
+	async doubleClick(selector) {
+		try {
+			await page.waitForSelector(selector)
+			await page.click(selector, { clickCount: 2 })
+		} catch (e) {
+			throw new Error(`Error al dar doble click en el selector ${selector}`)
+		}
+	}
+	async wait(time) {
+		await page.waitForTimeout(time)
+	}
+}
